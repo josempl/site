09107f53441c18d78fe7aff5c20ac436dbba9c62
jmpl <jmanuel.palacios@enreda.coop>
2023-07-05 09:38:06 +0200

    Add images and redesign footer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,11 @@
-// Add any custom javascript here.
+/* const navbar = document.querySelector('#nav-bar');
+let navbarPos = navbar.offsetTop + navbar.offsetHeight;
+function stickynavbar() {
+  console.log('scrollY', scrollY);
+  if (window.scrollY > navbarPos) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar); */
